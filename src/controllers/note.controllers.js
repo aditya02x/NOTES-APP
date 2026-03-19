a8a156297bcdebc2b889180582da575ea70ade9b
Aditya Raj <adityaraj34@icloud.com>
2026-03-19 13:51:27 +0530

    note controller done

--- src/controllers/note.controllers.js
+++ src/controllers/note.controllers.js
@@ -1,28 +1,32 @@
-import Note from "../models/Content.model.js";
+import Note from "../models/note.model.js";
 
 const createNote = async (req,res)=>{
     try {
         const {title , content} =req.body;
+
+        if(!title || !content){
+            return res.status(400).json({message:"Allfield are required"})
+        }
         const note = await Note.create({
             title,
             content,
-            user:req.user.id
+            user:req.user
 
         })
-        return resizeBy.status(201).json({
+        return res.status(201).json({
             message:"Note created Sucessfully",
             note
         })
         
     } catch (error) {
-        return resizeBy.status(500).json({message :"Server Error"})
+        return res.status(500).json({message :"Server Error"})
         
     }
 }
 
-const GetNotes = async (req,res)=>{
+const getNotes = async (req,res)=>{
     try {
-        const notes = await Note.find({user:req.user.is});
+        const notes = await Note.find({user:req.user});
         return res.status(200).json({notes})
         
     } catch (error) {
@@ -31,27 +35,29 @@ const GetNotes = async (req,res)=>{
     }
 }
 
-const deleteNote = async (req, res)=>{
+const deleteNote = async (req, res) => {
     try {
         const note = await Note.findById(req.params.id);
 
-        if(!note){
-            return res.status(404).json({message:"Note not found"})
+        // check if note exists
+        if (!note) {
+            return res.status(404).json({ message: "Note not found" });
         }
-        if(note.user.toString() !== req.user.id){
-            return res.status(403).json({mesaage:"Unauthorised"})
+
+        // check ownership
+        if (note.user.toString() !== req.user) {
+            return res.status(403).json({ message: "Unauthorized" });
         }
+
+        // delete note
         await note.deleteOne();
 
         return res.status(200).json({
-            message:"Note Deleted Sucessfully"
+            message: "Note deleted successfully"
         });
-        
-    } catch (error) 
-    {
-        return res.status(500).json({message:"Internal Server error"});
-        
-    }
-}
 
-export default {createNote,GetNotes}
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+export default {createNote,getNotes,deleteNote}
--- src/models/note.model.js
+++ src/models/note.model.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 const noteSchema = new mongoose.Schema({
     title:{
         type:String,
-        required:[true,"The title is required"]
+        required:[true,"The title is required"],
+        trim:true
     },
     content:{
         type:String,
-        required:[true,"The content is required"]
+        required:[true,"The content is required"],
+        trim:true
     },
     user:{
         type:mongoose.Schema.Types.ObjectId,
